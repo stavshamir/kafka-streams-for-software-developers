8f24cf4f7c32a56e551c3faa5c7a2bff7470ab52
Stav Shamir <shamir.stav@gmail.com>
2023-07-14 11:15:13 +0300

    Add topology test

--- stateless-topology/src/test/java/io/github/stavshamir/FeelingsTopologyTest.java
+++ stateless-topology/src/test/java/io/github/stavshamir/FeelingsTopologyTest.java
@@ -1,6 +1,5 @@
 package io.github.stavshamir;
 
-import io.github.stavshamir.serdes.FeelingSerde;
 import io.github.stavshamir.serdes.TrackSerializer;
 import io.github.stavshamir.types.Track;
 import org.apache.kafka.common.serialization.StringDeserializer;
@@ -16,11 +15,15 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class FeelingsTopologyTest {
 
+    private final Topology feelingsTopology = FeelingsTopology.buildTopology();
 
     @Test
-    void name() {
-        Topology topology = FeelingsTopology.buildTopology();
-        var topologyTestDriver = new TopologyTestDriver(topology);
+    void instrumentalTracksWithListeningTimeOverThreshold() {
+        // Given an instrumental track with listening time over 30 seconds
+        var track = new Track(UUID.randomUUID(), 20, true);
+
+        // When the track is processed
+        var topologyTestDriver = new TopologyTestDriver(feelingsTopology);
 
         var tracksTopic = topologyTestDriver
                 .createInputTopic("tracks", new VoidSerializer(), new TrackSerializer());
@@ -28,10 +31,20 @@ class FeelingsTopologyTest {
         var feelingsTopic = topologyTestDriver
                 .createOutputTopic("feelings", new VoidDeserializer(), new StringDeserializer());
 
-        tracksTopic.pipeInput(new Track(UUID.randomUUID(), 200, true));
-//        feelingsTopic.readValuesToList();
-        System.out.println(feelingsTopic.readValuesToList());
+        tracksTopic.pipeInput(track);
+
+        // Then the feelings queue will have five records
+        assertEquals(5, feelingsTopic.getQueueSize());
     }
 
+    @Test
+    void instrumentalTracksWithListeningTimeBelowThreshold() {
+        // Given an instrumental track with listening time below 30 seconds
+        var track = new Track(UUID.randomUUID(), 5, true);
+
+        // When the track is processed
+
+        // Then the feelings queue will be empty
+    }
 
 }
